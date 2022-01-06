34dd635db4cc8b63abc7c23aab65dac8e1f96d29
Erick Kiminza <kiminzaeric@gmail.com>
2022-01-06 12:35:44 +0300

    limit function lines to 5

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,6 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(
-                        field + "=.*?" + separator,
-                        field + "=" + redaction + separator, message)
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
     return message
