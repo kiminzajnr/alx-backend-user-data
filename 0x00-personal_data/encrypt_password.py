61c90c23c58252de033b15c58549ea11c2d89676
Erick Kiminza <kiminzaeric@gmail.com>
2022-01-06 22:01:15 +0300

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,11 @@ def hash_password(password: str) -> bytes:
     """returns a hashed password"""
     hashed = bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validates that provided password matches
+    the hashed password"""
+    if bcrypt.checkpw(bytes(password, 'utf-8'), hashed_password):
+        return True
+    return False
