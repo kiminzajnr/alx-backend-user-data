d3507cb66b57bd80a453af795697036074ecc98a
Erick Kiminza <kiminzaeric@gmail.com>
2022-01-15 21:14:23 +0300

    use abort to test my handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,6 +13,11 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def test_unauthorized():
+    """for testing unauthorized error
+    """
+    abort(401)
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
