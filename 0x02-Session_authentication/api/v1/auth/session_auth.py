b966aee6f370c5b88cf7228b60e66be0d8d1d10e
Erick Kiminza <kiminzaeric@gmail.com>
2022-01-21 22:30:50 +0300

    empty class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" SessionAuth inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ implements SessionAuth authentication mechanism
+    """
