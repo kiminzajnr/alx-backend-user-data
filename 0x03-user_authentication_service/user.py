a8733debf0ddbec23d9523aac6b8411237be89ac
Erick Kiminza <kiminzaeric@gmail.com>
2022-01-30 22:23:31 +0300

    document my module

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,6 +8,9 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ sqlalchemy model named User for a database
+    table named users
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
